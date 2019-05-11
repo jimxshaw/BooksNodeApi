86d50c954a4c7f37f244f76afde6df3b93df908a
Jim Shaw <jimxshaw@gmail.com>
2019-05-11 16:11:34 -0500

    Reformat document after using linting.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my api!");
+app.get('/', (req, res) => {
+  res.send('Welcome to my api!');
 });
 
 app.listen(port, () => {
-  console.log("Running on port " + port);
+  console.log(`Running on port ${port}`);
 });
