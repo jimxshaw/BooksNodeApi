674fe7158e25f3ae23e4ce5fa1d1d5135d00c344
Jim Shaw <jimxshaw@gmail.com>
2019-05-12 10:52:32 -0500

    Add books json file.

---
+++ booksJson.js
@@ -0,0 +1,38 @@
+db.books.insert([
+  {
+    title: "The Hobbit",
+    genre: "Fantasy",
+    author: "J. R. R. Tolkein",
+    read: true
+  },
+  {
+    title: "How to Win Friends and Influence People",
+    genre: "Self Help",
+    author: "Dale Carnegie",
+    read: false
+  },
+  {
+    title: "War and Peace",
+    genre: "Fiction",
+    author: "Lev Tolstoy",
+    read: false
+  },
+  {
+    title: "Memoirs of General William T. Sherman",
+    genre: "Nonfiction",
+    author: "William T. Sherman",
+    read: false
+  },
+  {
+    title: "Freakonomics",
+    genre: "Nonfiction",
+    author: "Stephen Dubner",
+    read: false
+  },
+  {
+    title: "Dune",
+    genre: "Science Fiction",
+    author: "Frank Herbert",
+    read: true
+  }
+]);
