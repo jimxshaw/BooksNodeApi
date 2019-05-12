4924cd316d256de7f1ab4b93e91c34e07da3f292
Jim Shaw <jimxshaw@gmail.com>
2019-05-12 11:11:53 -0500

    Add a book model.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+const db = mongoose.connect("mongodb://localhost/booksAPI");
+
 const port = process.env.PORT || 8080;
 const bookRouter = express.Router();
+
+const Book = require("./models/bookModel");
 
 bookRouter.route("/books").get((req, res) => {
   const response = { hello: "This is my API" };
